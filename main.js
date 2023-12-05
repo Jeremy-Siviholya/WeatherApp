8a7b253bff6a5ad1173967e2757560bca269cd83
Jeremy-Siviholya <jeremysiviholya7@outlook.fr>
2023-12-05 15:21:54 +0100

    use data on front

--- main.js
+++ main.js
@@ -1,7 +1,27 @@
-const city=document.querySelector('input').value
-const apiKey = "133e179b19cc4c4ab0311edfc8a233f5";
-const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=133e179b19cc4c4ab0311edfc8a233f5`;
+const city=document.querySelector('input')
+const degre = document.querySelector(".degre");
+const names=document.querySelector('.name')
+const wind=document.querySelector('.wind')
+const humidity=document.querySelector('.humidity')
+async function checkWheather (city){
+    const apiKey = "133e179b19cc4c4ab0311edfc8a233f5";
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city},uk&APPID=${apiKey}`;
+    const response=await fetch(apiUrl)
+    const data=await response.json()
 
-const response=await fetch(apiUrl)
-const data=await response.json()
-console.log(data);
+    console.log(data);
+
+    degre.innerHTML = `${Math.round(data.main.temp)}<sup>o</sup>C`;
+    names.innerHTML=data.name
+    wind.innerHTML=`${data.wind.speed} km/h`
+    humidity.innerHTML=`${data.main.humidity}%`
+
+}
+document.querySelector('img').addEventListener('click',
+()=>{
+
+    checkWheather(city.value)
+}
+)
+
+
